d192f308a3a98bc63ac2202dc74cb7626a16ffa7
Jonathan Mallett <jonnmallett@gmail.com>
2017-12-10 17:19:37 +0100

    Added gets mehtod file

---
+++ src/Section2/gets_method.rb
@@ -0,0 +1,7 @@
+puts "Hi, what's your name?"
+name = gets.chomp
+
+puts "Great! What's your age?"
+age = gets.chomp.to_i
+
+puts "Nice! So your name is #{name} and you are #{age} years old."
---
+++ src/Section2/string_interpolation.rb
@@ -0,0 +1,12 @@
+# Convert an integer to a string
+p 5.to_s
+
+name = 'Jonathan'
+
+p "Hello #{name}, how are you?"
+
+age = 28
+
+p "I am #{age} years old."
+
+p "The result of adding 1 + 1 is #{1 + 1}"
