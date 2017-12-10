5360c68efa446a72ef9f0b5ceea1c808772170b1
Jonathan Mallett <jonnmallett@gmail.com>
2017-12-10 16:00:44 +0100

    Added comments and variable files

---
+++ src/Section2/comments.rb
@@ -0,0 +1,17 @@
+# This is a comment
+
+puts 'Hello world' # Prints "Hello world" to the console
+
+# In this section of code,
+# we are going to do operations
+# A, B and C
+
+=begin
+This can be used for multi line comments. 
+It is not commonly used
+
+USe ctrl + / for quickly commenting out lines
+=end
+
+# puts 1
+# puts 2
