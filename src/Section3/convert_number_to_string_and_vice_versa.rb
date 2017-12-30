ea12eea08f61bcb4eff99fff4bfac5a5a242a7ce
Jonathan Mallett <jonnmallett@gmail.com>
2017-12-30 12:45:36 +0100

    Added number conversion class

---
+++ src/Section3/convert_number_to_string_and_vice_versa.rb
@@ -0,0 +1,30 @@
+# Converting string to int and float
+# str = '5.8'
+# p str
+# p str.class
+
+# p str.to_i
+# p str.to_i.class
+
+# p str.to_f
+# p str.to_f.class
+
+# Converting int to string and to float
+# number = 10
+
+# p number.class
+# p number.to_s
+# p number.to_s.class
+
+# p number.to_f
+# p number.to_f.class
+
+# Converting float to int and string
+pi = 3.14
+
+p pi.class
+p pi.to_i
+p pi.to_i.class
+
+p pi.to_s
+p pi.to_s.class
