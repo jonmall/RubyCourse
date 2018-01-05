8c0eef81fd6aca30a909fdf2dfb6467fe409b9d2
Jonathan Mallett <jonnmallett@gmail.com>
2018-01-05 14:15:17 +0100

    More code samples

---
+++ src/Section3/intro_to_booleans.rb
@@ -0,0 +1,26 @@
+p 5 < 10
+
+p 1.odd?
+p 2.even?
+
+p 10 == 10.0
+p 10 == 20
+
+a = 10
+b = 5
+c = 10
+
+p a == c
+p a == b
+p b == c
+
+p b.to_f == 5.0
+
+p a != b
+p a != c
+
+p "hello" != "goodbye"
+p "hello" == "hello"
+p "HeLlO".upcase == "hello".upcase
+
+p 123 != "123"
---
+++ src/Section3/methods.rb
@@ -0,0 +1,11 @@
+p 20.between?(10, 30)
+p 20.between?(21, 25)
+p 1.2.between?(1.0, 5)
+
+p 10.5.floor
+p 10.3.ceil
+
+p 3.14159.round(3)
+
+p 35.67.abs
+p -35.67.abs
